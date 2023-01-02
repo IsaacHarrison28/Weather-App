e61f2e32b84b83b64f486f5b7055d50c8967e4f3
Isaac Harrison <sateonlineservices@gmail.com>
2023-01-02 02:02:33 -0800

    finished product

--- APIkey.js
+++ APIkey.js
@@ -1,1 +1,3 @@
-key = "b933ba79a5cd773b71acb1b46b47792f"
+const APIkey = "a3c276c5a41323e108f54ec3ccd38063";
+
+export default APIkey;
--- myjavascript.js
+++ myjavascript.js
@@ -1,50 +1,46 @@
 let result = document.getElementById('result');
 let searchBtn = document.getElementById('search-btn');
-let cityRef = document.getElementById('city').value;
+let cityRef = document.getElementById('city');
+import key from './APIkey.js';
 
-// let getWeather = ()  => {
+let getWeather = ()  => {
+    
+    let cityValue = cityRef.value
+    if(cityValue.length == 0){
+        result.innerHTML = `<h5>Please Enter a City Name</h5>`
+    }
+    else{
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+        
+        fetch(url)
+        .then((resp) => resp.json())
+        .then((data) => {
+            
+            result.innerHTML = `
+            <h2>${data.name}</h2>
+            <h4 class="weather">${data.weather[0].main}</h4>
+            <h4 class="desc">${data.weather[0].description}</h4>
+            <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+            <h1>${data.main.temp} &#176;</h1>
+            <div class="temp-container">
+                <div>
+                    <h4 class="title">Min</h4>
+                    <h4 class="temp">${data.main.temp_min}</h4>
+                </div>
+                <div>
+                    <h4 class="title">Max</h4>
+                    <h4 class="temp">${data.main.temp_max}</h4>
+                </div>
+            </div>
+            `
+            cityValue = " "
+        })
+        .catch(()=> {
+            result.innerHTML = `<h3 class="msg">City Not Found</h3>`;
+        })
+    }
+};
 
-//     if(cityRef.length == 0){
-//         result.innerHTML = `<h5>Please Enter a City Name</h5>`
-//     }
-//     else{
-//         let url = 'https://api.openweathermap.org/data/2.5/weather?q=${cityRef}&appid=${APIkey}&units=metric';
-//         cityRef = ""
-//         fetch(url)
-//         .then((resp) => resp.json())
-//         .then((data) => {
-//             //I must remove this once done with everything
-//             console.log(data);
-//             console.log(data.weather[0].icon);
-//             console.log(data.weather[0].main);
-//             console.log(data.weather[0].description);
-//             console.log(data.name);
-//             console.log(data.main.temp_min);
-//             console.log(data.main.temp_max);
-//             result.innerHTML = `
-//             <h2>${data.name}</h2>
-//             <h4 class="weather">${data.weather[0].main}</h4>
-//             <h4 class="desc">${data.weather[0].description}</h4>
-//             <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
-//             <h1>${data.main.temp} &#176;</h1>
-//             <div class="temp-container">
-//                 <div>
-//                     <h4 class="title">Min</h4>
-//                     <h4 class="temp">${data.main.temp_min}</h4>
-//                 </div>
-//                 <div>
-//                     <h4 class="title">Max</h4>
-//                     <h4 class="temp">${data.main.temp_max}</h4>
-//                 </div>
-//             </div>
-//             `
-//         })
-//         .catch(()=> {
-//             result.innerHTML = `<h3 class="msg">City Not Found</h3>`;
-//         })
-//     }
-// };
-
-// searchBtn.addEventListener('click', getWeather)
-// window.addEventListener('load', getWeather);
+searchBtn.addEventListener('click', getWeather)
+window.addEventListener('load', getWeather);
 
